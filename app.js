3b85af081efddbac161c86ed3c32f8435991e0be
Joe Chapman <contemno82@gmail.com>
2017-08-24 10:57:58 +0100

    m

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const favicon = require('serve-favicon');
 
 const dbConnect = require(path.join(__dirname, 'app/model/init'));
 
-app.use(favicon(path.join(__dirname, 'app/assets/images', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'app/assets/images', 'favicon.ico')));
 app.use('/api', api);
 api.use(bodyParser.json({limit: '5mb', parameterLimit: 400000})); // limit based on full case load
 api.use(bodyParser.urlencoded( {limit: '5mb', parameterLimit: 400000, extended : false} ));
@@ -20,8 +20,8 @@ dbConnect();
 require(path.join(__dirname, 'app/routes/routes'))(api);
 
 app.get('/', (req, res) => res.send('running'));
-app.get('*', (req, res) => res.status(404).send('Not found'));
-app.all('*', (req, res) => res.status(405).send('Method not allowed'));
+// app.get('*', (req, res) => res.status(404).send('Not found'));
+// app.all('*', (req, res) => res.status(405).send('Method not allowed'));
 
 app.listen(PORT, () => {
     console.log('Hey, Listen!');
